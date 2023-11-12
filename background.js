e080b4dca095831359e5bd42fce392074ef03358
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-11-12 11:03:51 -0800

    IOS Safari extension setup

--- background.js
+++ background.js
@@ -1,20 +1,13 @@
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
-    var currentTab = tabs[0]; // there will be only one in this array
-    console.log(currentTab.url); // this is the URL of the current tab
-  });
 
-chrome.history.search({ text: '', maxResults: 10 }, function(data) {
-  data.forEach(function(page) {
-    console.log(page.url);
-  });
-});
+// Event listener for messages from the content script
+safari.application.addEventListener("message", handleMessage, false);
 
-chrome.history.search({ text: '', maxResults: 10 }, function(historyItems) {
-    historyItems.forEach(function(item) {
-        var img = document.createElement('img');
-        img.src = 'http://www.google.com/s2/favicons?domain_url=' + encodeURIComponent(item.url);
-        img.alt = 'Favicon';
-        document.body.appendChild(img);
-      });
-      
-  });
+function handleMessage(event) {
+  if (event.name === "SaveURL") {
+    let url = event.message.url;
+    // Save the URL using Safari's storage API
+    safari.extension.settings.setItem('lastURL', url);
+    console.log(url);
+  }
+}
+
--- popup.js
+++ popup.js
@@ -1,51 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
-    chrome.history.search({ text: '', maxResults: 10 }, function (historyItems) {
-      var sitesList = document.getElementById('sites-list');
-  
-      historyItems.forEach(function (item) {
-        // Create a function to fetch the high-res favicon
-        function fetchFavicon(url) {
-          // Assuming you have permission to make cross-origin requests to the domain
-          fetch(url).then(response => response.text()).then(html => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(html, 'text/html');
-            const iconLink = doc.querySelector('link[rel="icon"][sizes="192x192"], link[rel="icon"][sizes="180x180"], link[rel="apple-touch-icon"]');
-  
-            return iconLink ? iconLink.href : 'http://www.google.com/s2/favicons?domain_url=' + encodeURIComponent(item.url);
-          }).catch(() => {
-            // Fallback if the fetch fails or icon not found
-            return 'http://www.google.com/s2/favicons?domain_url=' + encodeURIComponent(item.url);
-          }).then(faviconUrl => {
-            // Create list item with high-res favicon
-            var listItem = document.createElement('li');
-            var img = document.createElement('img');
-            var textContainer = document.createElement('div'); // Container for text and date
-            var titleSpan = document.createElement('span'); // Create a span for the title
-            titleSpan.classList.add('truncate'); // Add the truncate class
-            var titleText = document.createTextNode(item.title || item.url);
-            titleSpan.appendChild(titleText);
-            
-            // Create a div for the date
-            var dateDiv = document.createElement('div');
-            dateDiv.classList.add('date'); // Add the date class for styling
-            var visitDate = new Date(item.lastVisitTime); // Convert lastVisitTime to Date object
-            var dateText = document.createTextNode(visitDate.toLocaleDateString()); // Convert Date to a date-only string
-            dateDiv.appendChild(dateText);
+    const input = document.getElementById('input');
+    const button = document.getElementById('button');
+    const saveHistoryButton = document.getElementById('saveHistory');
+    const sitesList = document.getElementById('sites-list');
 
-            img.src = faviconUrl;
-            img.alt = 'Favicon';
+    // Function to create and append history item to list
+    function appendHistoryItem(item) {
+        var listItem = document.createElement('li');
+        var anchor = document.createElement('a');
+        anchor.href = item.url;
+        anchor.target = '_blank';
+        anchor.textContent = item.url;
+        anchor.classList.add('truncate');
+        listItem.appendChild(anchor);
+        sitesList.insertBefore(listItem, sitesList.firstChild); // Insert at the top
+    }
 
-            textContainer.appendChild(titleSpan); // Append the title span to the text container
-            textContainer.appendChild(dateDiv); // Append the date div to the text container
-            listItem.appendChild(img);
-            listItem.appendChild(textContainer); // Append the text container to the list item
-            sitesList.appendChild(listItem);
-          });
-        }
-  
-        // Call the fetchFavicon function with the URL of the history item
-        fetchFavicon(item.url);
+    // Append an example item for testing on load
+    appendHistoryItem({ url: 'http://example.com' });
+    appendHistoryItem({ url: "example" });
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // tabs[0] is the active tab in the current window
+        var currentURL = tabs[0].url;
+        
+        // Do something with the URL
+        appendHistoryItem({ url: currentURL });
+
+        console.log(currentURL);
       });
+    // Fetch history items and append to list initially
+    
+    // Search history when the button is clicked
+    // Save the current tab's URL when the "Save Current URL" button is clicked
+    saveHistoryButton.addEventListener('click', function () {
+        
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            // tabs[0] is the active tab in the current window
+            var currentURL = tabs[0].url;
+            
+            // Do something with the URL
+            appendHistoryItem({ url: currentURL });
+
+            console.log(currentURL);
+          });
     });
-  });
-  
+});
