1e94de6b1c91bba98607105163671d8adf55373b
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-11-24 18:23:19 -0800

    Change handler file

--- Web Time Machine/Shared (Extension)/SafariWebExtensionHandler.swift
+++ Web Time Machine/Shared (Extension)/SafariWebExtensionHandler.swift
@@ -13,26 +13,20 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
     func beginRequest(with context: NSExtensionContext) {
         let request = context.inputItems.first as? NSExtensionItem
 
-        let profile: UUID?
-        if #available(iOS 17.0, macOS 14.0, *) {
-            profile = request?.userInfo?[SFExtensionProfileKey] as? UUID
-        } else {
-            profile = request?.userInfo?["profile"] as? UUID
-        }
+        // Use a custom key for the profile UUID
+        let profileKey = "customProfileKey"
+        let profile: UUID? = request?.userInfo?[profileKey] as? UUID
 
-        let message: Any?
-        if #available(iOS 17.0, macOS 14.0, *) {
-            message = request?.userInfo?[SFExtensionMessageKey]
-        } else {
-            message = request?.userInfo?["message"]
-        }
+        // Use a custom key for the message
+        let messageKey = "customMessageKey"
+        let message: Any? = request?.userInfo?[messageKey]
 
         os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@ (profile: %@)", String(describing: message), profile?.uuidString ?? "none")
 
         let response = NSExtensionItem()
-        response.userInfo = [ SFExtensionMessageKey: [ "echo": message ] ]
+        // Use the same custom key for the response
+        response.userInfo = [ messageKey: [ "echo": message ] ]
 
         context.completeRequest(returningItems: [ response ], completionHandler: nil)
     }
-
 }
